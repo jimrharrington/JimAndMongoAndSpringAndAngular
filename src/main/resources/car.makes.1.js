6c034e5b83886079158693151e008bcb4059360e
jimrharrington <jim.r.harrington@gmail.com>
2017-01-25 14:34:03 -0500

    checkpoint

--- src/main/resources/car.makes.1.js
+++ src/main/resources/car.makes.1.js
@@ -1,3 +1,3 @@
 use test;
 
-db.car-makes.insertMany( {[ {"make_id":"abarth","make_display":"Abarth","make_is_common":"0","make_country":"Italy"} ]} );
+db.car-makes.insertMany( {[ {"make_id":"abarth","make_display":"Abarth","make_is_common":"1","make_country":"Italy"} ]} );
